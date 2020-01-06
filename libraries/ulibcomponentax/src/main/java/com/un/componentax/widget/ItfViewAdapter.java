fee08a97181ed929ab7b079572a6a2a18aa67fbf
unlim <xiangchh@puwell.com>
2020-01-06 18:05:44 +0800

    s

---
+++ libraries/ulibcomponentax/src/main/java/com/un/componentax/widget/ItfViewAdapter.java
@@ -0,0 +1,7 @@
+package com.un.componentax.widget;
+
+import android.view.View;
+
+public interface ItfViewAdapter<D> {
+	void adapter(View v, D data);
+}
--- libraries/ulibcomponentax/src/main/java/com/un/componentax/widget/LinearLayoutGroup.java
+++ libraries/ulibcomponentax/src/main/java/com/un/componentax/widget/LinearLayoutGroup.java
@@ -3,10 +3,15 @@ package com.un.componentax.widget;
 import android.content.Context;
 import android.graphics.Color;
 import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.LinearLayout;
 
 import androidx.annotation.Nullable;
+import androidx.cardview.widget.CardView;
 
+import com.un.utila.viewhelp.ViewClipUtil;
 import com.un.utilj.collect.MixCollectionUtil;
 
 import java.util.ArrayList;
@@ -30,6 +35,10 @@ public class LinearLayoutGroup extends LinearLayout {
 
 	public static class Build {
 
+		int resId;
+
+		ItfViewAdapter adapter;
+
 		Map<String, List> dataMap = new HashMap<>();
 		List<List> dataList = new ArrayList<>();
 
@@ -38,26 +47,53 @@ public class LinearLayoutGroup extends LinearLayout {
 			return this;
 		}
 
-		public void setAdapter() {
+		public Build setResId(int resId) {
+			this.resId = resId;
+			return this;
+		}
 
+		public Build setAdapter(ItfViewAdapter adapter) {
+			this.adapter = adapter;
+			return this;
 		}
 
 		public LinearLayoutGroup build(Context context) {
+			LayoutInflater li = LayoutInflater.from(context);
+
 			LinearLayoutGroup parent = new LinearLayoutGroup(context);
 			parent.setLayoutParams(new LinearLayout.LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT));
+			parent.setOrientation(LinearLayout.VERTICAL);
 			if (dataList.isEmpty()) {
 				return parent;
 			} else {
 				for (List childList : dataList) {
+					LinearLayout.LayoutParams ll = new LinearLayout.LayoutParams(
+							LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT
+					);
+					ll.setMargins(20, 20, 20, 20);
+
+					CardView cardView = new CardView(context);
+					cardView.setCardElevation(6);
+
+					parent.addView(cardView, ll);
+
 					LinearLayout groupView = new LinearLayout(context);
-					groupView.setBackgroundColor(Color.BLUE);
-					//					group.setLayoutParams(new LinearLayout.LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT));
-					parent.addView(groupView, new LinearLayout.LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT));
+					groupView.setOrientation(LinearLayout.VERTICAL);
+					groupView.setBackgroundColor(Color.WHITE);
+					ViewClipUtil.clipRound(groupView, 10);
+
+					cardView.addView(groupView, new ViewGroup.LayoutParams(
+									ViewGroup.LayoutParams.MATCH_PARENT,
+									ViewGroup.LayoutParams.WRAP_CONTENT
+							)
+					);
 
 					for (Object child : childList) {
-						LinearLayout childView = new LinearLayout(context);
-						childView.setBackgroundColor(Color.YELLOW);
-						groupView.addView(childView, new LinearLayout.LayoutParams(LayoutParams.MATCH_PARENT, 50));
+						View v = li.inflate(resId, groupView, false);
+						if (adapter != null) {
+							adapter.adapter(v, child);
+						}
+						groupView.addView(v, new LinearLayout.LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT));
 					}
 				}
 				return parent;
