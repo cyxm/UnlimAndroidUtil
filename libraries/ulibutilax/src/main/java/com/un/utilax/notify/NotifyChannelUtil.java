8ae5830ffaa26ce2220f1d3751a11a5325078fad
unlim <xiangchh@puwell.com>
2020-06-03 20:18:10 +0800

    增加通知渠道工具

---
+++ libraries/ulibutilax/src/main/java/com/un/utilax/notify/NotifyChannelUtil.java
@@ -0,0 +1,35 @@
+package com.un.utilax.notify;
+
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.content.Context;
+
+public class NotifyChannelUtil {
+
+	/**
+	 * 创建通知渠道
+	 *
+	 * @param context
+	 * @param channelId
+	 * @param channelName
+	 * @param channelDes
+	 */
+	public static void createChannel(Context context, String channelId, String channelName, String channelDes) {
+		if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+
+			int importance = NotificationManager.IMPORTANCE_DEFAULT;
+			NotificationChannel mChannel = new NotificationChannel(channelId, channelName, importance);
+
+			mChannel.setDescription(channelDes);//渠道描述
+
+			NotificationManager notificationManager = (NotificationManager) context.getSystemService(
+					Context.NOTIFICATION_SERVICE
+			);
+			if (notificationManager == null) {
+				return;
+			}
+			notificationManager.createNotificationChannel(mChannel);
+		}
+	}
+
+}
---
+++ libraries/ulibutilax/src/main/java/com/un/utilax/notify/NotifyUtil.java
@@ -0,0 +1,9 @@
+package com.un.utilax.notify;
+
+public class NotifyUtil {
+
+	public static void norify() {
+
+	}
+
+}
