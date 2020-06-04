e78eb5ff99f3336bae5486b14636d95dcb1dc1ba
unlim <xiangchh@puwell.com>
2020-06-04 20:39:23 +0800

    s

--- libraries/ulibutilj/src/main/java/com/un/utilj/net/IpAddrUtil.java
+++ libraries/ulibutilj/src/main/java/com/un/utilj/net/IpAddrUtil.java
@@ -4,10 +4,28 @@ public class IpAddrUtil {
 
 	public static int str2Int(String ip) {
 		String[] mIpArr = ip.split("\\.");
+
 		int ipint = 0;
-		for (int i = mIpArr.length - 1; i >= 0; i--) {
-			ipint = (ipint << 8) | Integer.valueOf(mIpArr[i]);
+
+		if (mIpArr.length != 4) {
+			return ipint;
 		}
+
+		try {
+			for (int i = mIpArr.length - 1; i >= 0; i--) {
+				ipint = (ipint << 8) | Integer.parseInt(mIpArr[i]);
+			}
+		} catch (Exception e) {
+			ipint = 0;
+		}
+
 		return ipint;
+	}
+
+	public static String intToStr(int i) {
+		return (i & 0xFF) + "." +
+				((i >> 8) & 0xFF) + "." +
+				((i >> 16) & 0xFF) + "." +
+				(i >> 24 & 0xFF);
 	}
 }
