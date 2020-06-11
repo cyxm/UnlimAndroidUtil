9409023e90c777dd6d940b2fa5d91fd8e4c01585
unlim <xiangchh@puwell.com>
2020-06-11 20:41:27 +0800

    s

--- libraries/ulibutilax/src/main/java/com/un/utilax/notify/NotifyUtil.java
+++ libraries/ulibutilax/src/main/java/com/un/utilax/notify/NotifyUtil.java
@@ -1,9 +1,20 @@
 package com.un.utilax.notify;
 
+import android.content.Context;
+
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
+
 public class NotifyUtil {
 
-	public static void norify() {
+	public static void norify(Context context, String channelId, int iconRes, String title, String content) {
+		NotificationCompat.Builder builder = new NotificationCompat.Builder(context, channelId);
+		builder.setSmallIcon(iconRes)
+				.setContentTitle(title)
+				.setContentText(content)
+				.build();
 
+		NotificationManagerCompat.from(context).notify(1, builder.build());
 	}
 
 }
