4abb0a18b5a5f866e1f3e7a45cc2650aab877654
unlim <xiangchh@puwell.com>
2020-07-20 17:50:52 +0800

    s

--- libraries/ulibcomponentax/src/main/java/com/un/componentax/component/ComponentSysUtil.java
+++ libraries/ulibcomponentax/src/main/java/com/un/componentax/component/ComponentSysUtil.java
@@ -2,11 +2,16 @@ package com.un.componentax.component;
 
 import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Build;
 import android.provider.MediaStore;
 
+import androidx.core.content.FileProvider;
+
 import com.un.componentax.act.ActivityBase;
 import com.un.componentax.act.IOnActivityResult;
+
+import java.io.File;
 
 public class ComponentSysUtil {
 
@@ -37,5 +42,24 @@ public class ComponentSysUtil {
 	 */
 	public static void goWifiSetting(Context context) {
 		context.startActivity(new Intent(android.provider.Settings.ACTION_WIFI_SETTINGS));
+	}
+
+	public static void goShare(Context context, String title, String intentType, String authority, String path) {
+		Intent intent = new Intent();
+		intent.setAction(Intent.ACTION_SEND);
+
+		Uri uri;
+		if (Build.VERSION.SDK_INT >= 24) {
+			uri = FileProvider.getUriForFile(context, authority, new File(path));
+			intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+		} else {
+			uri = Uri.fromFile(new File(path));
+		}
+		intent.putExtra(Intent.EXTRA_STREAM, uri);
+		intent.setType(intentType);
+
+		Intent shareIntent = Intent.createChooser(intent, title);
+
+		context.startActivity(shareIntent);
 	}
 }
---
+++ libraries/ulibcomponentax/src/main/java/com/un/componentax/component/IntentType.java
@@ -0,0 +1,7 @@
+package com.un.componentax.component;
+
+public class IntentType {
+	public static final String TEXT = "text/plain";
+	public static final String VIDEO = "video/*";
+	public static final String IMAGE = "image/*";
+}
